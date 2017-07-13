4ebf707343a41a17902f99125ad17a189af20604
tangzhankun <zhankun.tang@intel.com>
2017-07-14 03:32:18 +0800

    delete un-used files

--- clear.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-iptables-save | awk '/^[*]/ { print $1 } 
-                     /^:[A-Z]+ [^-]/ { print $1 " ACCEPT" ; }
-                                          /COMMIT/ { print $0; }' | iptables-restore
