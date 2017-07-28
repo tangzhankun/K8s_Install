3002964cee404c16d80995b962861d557d03dbdd
tangzhankun <zhankun.tang@intel.com>
2017-07-28 12:49:51 +0800

    custom docker file

--- jd-dev-test-spark.sh
+++ jd-dev-test-spark.sh
@@ -13,10 +13,9 @@
   --conf spark.executorEnv.USER_NAME=calico1 \
   --conf spark.kubernetes.driverEnv.user=calico1 \
   --conf spark.kubernetes.driverEnv.USER_NAME=calico1 \
-  --conf spark.kubernetes.driver.docker.image=172.16.3.78:4000/spark-driver:wip \
-  --conf spark.kubernetes.executor.docker.image=172.16.3.78:4000/spark-executor:wip \
-  --conf spark.kubernetes.initcontainer.docker.image=172.16.3.78:4000/spark-init:wip \
+  --conf spark.kubernetes.driver.docker.image=172.16.3.78:4000/spark-driver:jdv1 \
+  --conf spark.kubernetes.executor.docker.image=172.16.3.78:4000/spark-executor:jdv1 \
+  --conf spark.kubernetes.initcontainer.docker.image=172.16.3.78:4000/spark-init:jdv1 \
   --conf spark.kubernetes.driver.labels="user=calico1" \
   --conf spark.kubernetes.executor.labels="user=calico1" \
-  --conf spark.kubernetes.docker.image.pullPolicy="Always" \
-  local:///opt/spark/examples/jars/spark-examples_2.11-2.1.0-k8s-0.3.0-SNAPSHOT.jar 10 160000 4000
+  local:///opt/spark/examples/jars/spark-examples_2.11-2.1.0-k8s-0.2.0-SNAPSHOT.jar 10 640000 4000
--- test-spark.sh
+++ test-spark.sh
@@ -1,4 +1,5 @@
-#!/bin/bash 
+#!/bin/bash
+
 /root/spark-on-k8s/dist/bin/spark-submit \
   --deploy-mode cluster \
   --class org.apache.spark.examples.GroupByTest \
@@ -8,11 +9,11 @@
   --conf spark.app.name=GroupByTest \
   --conf spark.executor.instances=3 \
   --conf spark.executor.cores=4 \
-  --conf spark.executor.memory=2g \
+  --conf spark.executor.memory=4g \
   --conf spark.executorEnv.user=calico1 \
   --conf spark.kubernetes.driver.docker.image=172.16.3.78:4000/spark-driver:jd \
   --conf spark.kubernetes.executor.docker.image=172.16.3.78:4000/spark-executor:jd \
   --conf spark.kubernetes.initcontainer.docker.image=172.16.3.78:4000/spark-init:jd \
   --conf spark.kubernetes.driver.labels="user=calico1" \
   --conf spark.kubernetes.executor.labels="user=calico1" \
-  local:///opt/spark/examples/jars/spark-examples_2.11-2.1.0-k8s-0.2.0-SNAPSHOT.jar 10 400000 2
+  local:///opt/spark/examples/jars/spark-examples_2.11-2.1.0-k8s-0.2.0-SNAPSHOT.jar 10 400000 128
